8c63d9c238a42ed6c079facec583f14dd7ba1c1b
badeasalameh <130796009+badeasalameh@users.noreply.github.com>
2024-03-27 23:25:55 +0200

    Delete ex4 directory

--- ex4/ex4/PotionMerchant.cpp
+++
@@ -1,6 +0,0 @@
-#include "PotionMerchant.h"
-PotionMerchant :: PotionMerchant() : Event(){}
-string PotionMerchant :: getDescription() const
-{
-    return "PotionMerchant";
-}
--- ex4/ex4/SolarEclipse.cpp
+++
@@ -1,6 +0,0 @@
-#include "SolarEclipse.h"
-string SolarEclipse :: getDescription() const
-{
-    return "SolarEclipse";
-}
-SolarEclipse ::SolarEclipse() : Event(){}
