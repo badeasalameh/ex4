4d6c33c5e706b0e0b8e03dab6432a6d78096b242
mahmoudabu19 <159143198+mahmoudabu19@users.noreply.github.com>
2024-03-30 23:08:59 +0300

    Delete ex4/ex4 directory

--- ex4/ex4/Cards/Dragon.cpp
+++
@@ -1,9 +0,0 @@
-#include "Dragon.h"
-using std::string;
-
-Dragon ::Dragon() : Encounter(INITIAL_DRAGON_COMBATPOWER , INITIAL_DRAGON_LOOT , INITIAL_DRAGON_DAMAGE)
-{}
-string Dragon ::getDescription() const
-{
-    return "Dragon (power " + std::to_string(m_combatPower) + ", loot " + std::to_string(m_loot) + ", damage " + std::to_string(m_damage) + ")";
-}
--- ex4/ex4/Sorcerer.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Mahmoud on 3/20/2024.
-//
-
-#include "Sorcerer.h"
-Sorcerer :: Sorcerer(const string& name ,const string& behavior,int maxHP,int force): Player(name,"Sorcerer",behavior,maxHP,force){}
-int Sorcerer::getAttackStrength() const  {
-    return m_force+m_level;
-}
