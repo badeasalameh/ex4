f63e527af5bcc4a8eaf70c9f306a022dfa7c8934
badeasalameh <130796009+badeasalameh@users.noreply.github.com>
2024-03-31 16:01:37 +0300

    Delete ex4 directory

--- ex4/Dragon.cpp
+++
@@ -1,7 +0,0 @@
-#include "Dragon.h"
-Dragon ::Dragon() : Encounter(INITIAL_DRAGON_COMBATPOWER , INITIAL_DRAGON_LOOT , INITIAL_DRAGON_DAMAGE)
-{}
-string Dragon ::getDescription() const
-{
-    return "Dragon (power " + to_string(m_combatPower) + ", loot " + to_string(m_loot) + ", damage " + to_string(m_damage) + ")";
-}
--- ex4/Sorcerer.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Mahmoud on 3/20/2024.
-//
-
-#include "Sorcerer.h"
-
-int Sorcerer::getAttackStrength() const  {
-    return m_force+m_level;
-}
