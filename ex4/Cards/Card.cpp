531a03a3434a90cf25e7294a5a85f286b45f7e8f
mahmoudabu19 <159143198+mahmoudabu19@users.noreply.github.com>
2024-04-01 11:52:45 +0300

    Delete ex4 directory

--- ex4/Cards/Card.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Mahmoud on 3/19/2024.
-//
-#include "Card.h"
-Card :: Card(const string& cardType) : m_cardType(cardType){}
--- ex4/Cards/Event.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by Mahmoud on 3/24/2024.
-//
-
-#include "Event.h"
-
-Event :: Event() : Card("Event"){}
